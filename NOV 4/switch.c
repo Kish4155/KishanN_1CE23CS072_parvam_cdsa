1bdbd5505d669aa50c64998bd56ba4c687ee3ce7
Kish4155 <kishunachar@gmail.com>
2024-11-06 14:17:50 +0530

    Add files via upload

---
+++ NOV 4/switch.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    switch(n)
+    {
+        case 1:printf("Mon");
+        break;
+        case 2: printf("tue");
+        break;
+        case3:printf("Wed");
+        break;
+        default:printf("invalid");
+
+    }
+    
+}
